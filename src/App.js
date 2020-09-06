dc9535579dce0c5a39de3122ebd444a2160787c6
Shuvam Nandi <shuvamnandi9@gmail.com>
2020-09-06 21:03:13 +0800

    React Hook example in Module 3.44

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 
 import './App.css';
 
-class App extends Component {
-  // React Hooks allows managing state in functional components post React 16.8
-  // For now, we will use class to manage state
+function App (props) {
   // If the state changes, it will make React re-render our DOM
-  state = {
-    persons: [ 
-      { name: 'Shuvam', age: 25 }, 
-      { name: 'Neethu', age: 24 }, 
-      { name: 'Parth', age: 26 }, 
-    ],
-    otherState: 'Some value'  
-  }
+  // For now, we will use class to manage state
+  
+  // React Hooks allows managing state in functional components post React 16.8
+  // useState is a React Hook used for managing state in functional components
 
-  switchNameHandler = event => {
+  const [ personsState, setPersonsState ] = useState( {
+      persons: [ 
+        { name: 'Shuvam', age: 25 }, 
+        { name: 'Neethu', age: 24 }, 
+        { name: 'Parth', age: 26 }, 
+      ]
+    }
+  );
+
+  const [ otherState, setOtherState ] = useState( 'Some value' );
+
+  const switchNameHandler = event => {
     console.log('Was clicked!', event.target);
     //this.state.persons[0].name=  'Max'; // DO NOT DO THIS, React does not accept this
-    this.setState( {
+    // Does not merge it with existing state, therefore deletings otherState
+    setPersonsState( {
       persons: [
         { name: 'Shuvam', age: 26 }, 
         { name: 'Neethu', age: 25 }, 
         { name: 'Parth', age: 27 }, 
-    ]
+      ]
   });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>I am a React Application</h1>
-        <h2>Hello World!</h2>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
-          My hobbies None
-        </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>I am a React Application</h1>
+      <h2>Hello World!</h2>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Names</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>
+        My hobbies None
+      </Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
   }
   // return (
   //   <div className="App">
@@ -55,6 +60,27 @@ class App extends Component {
   //   React.createElement('h1', null, 'I am a React App' ), 
   //   React.createElement('h2', null, 'Hello World!')
   // );
-}
 
 export default App;
+
+// state = {
+//   persons: [ 
+//     { name: 'Shuvam', age: 25 }, 
+//     { name: 'Neethu', age: 24 }, 
+//     { name: 'Parth', age: 26 }, 
+//   ],
+//   otherState: 'Some value'  
+// }
+
+// switchNameHandler = event => {
+//   console.log('Was clicked!', event.target);
+//   //this.state.persons[0].name=  'Max'; // DO NOT DO THIS, React does not accept this
+//   // Merges it with existing state, therefore retaining otherState
+//     this.setState( {
+//     persons: [
+//       { name: 'Shuvam', age: 26 }, 
+//       { name: 'Neethu', age: 25 }, 
+//       { name: 'Parth', age: 27 }, 
+//   ]
+// });
+// }
